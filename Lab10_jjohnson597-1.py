4775167f6d0d99fd07447ca066f733c79ee39573
jjohnson597 <jjohnson597@student.cscc.edu>
2026-07-05 11:48:41 -0400

    Create: Add program header and required imports

---
+++ Lab10_jjohnson597-1.py
@@ -0,0 +1,11 @@
+"""
+Program: Lab 10 Word Analyzer
+Author: Jaylen Johnson
+Purpose: Analyze selected text files and count word frequencies using OOP, pathlib, and string processing.
+Starter code: No starter code was used.
+Date: 7/5/2026
+"""
+
+from pathlib import Path
+import string
+
